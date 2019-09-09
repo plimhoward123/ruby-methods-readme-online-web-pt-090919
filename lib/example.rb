1a4a10c2b37e4960945b3cd8e68c96a45ea06b7b
Plimon Howard V <plimon.howard@gmail.com>
2019-09-09 17:02:39 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,15 @@
-# Use this space to code along with the readme
 def greeting
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 
 say_hello_world_ten_times
